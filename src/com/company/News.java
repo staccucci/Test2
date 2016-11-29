35b90f1570a710be46b25e55b155489c9658be9f
sylvain <sylvain.taccucci@gmail.com>
2016-11-29 06:08:24 -0500

    Fichier interface déplacé dans le bon package

---
+++ src/com/company/News.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+
+/**
+ * Created by sylvain on 2016-11-29.
+ * @author sylvain
+ * @
+ * Cette classe permet d'obtenir une liste de nouvelles au sujet
+ * d'une moto en particulier.
+ * Les nouvelles obtenues le sont d'internet
+ */
+public class News {
+    private List<String> new;
+
+    private List<String> searchParameters;
+
+    /**
+     *
+     * @param _searchParameters: Liste de paramètres de recherche pour obtenir des nouvelles sur un modèle
+     */
+    public void News(List<String> _searchParameters) {
+        searchParameters = _searchParameters;
+
+    }
+
+
+}
