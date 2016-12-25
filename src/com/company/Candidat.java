76d61cc988985bc08ba4037963042722098ab4f5
Sylvain Taccucci <sylvain.taccucci@gmail.com>
2016-12-25 14:49:15 -0500

    Ajout d'une classe pour les motos candidates pour un achat

---
+++ src/com/company/Candidat.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Sylvain on 2016-12-25.
+ *
+ * représente une classe utilisé pour être dans les motos candidates pour
+ * un achat
+ */
+public class Candidat {
+}
