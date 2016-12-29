50f1413a0dd69058d61fb5594304b79a49582d10
Sylvain Taccucci <staccucci@Shadow.local>
2016-12-29 15:42:36 -0500

    Ajustement d'un commentaire

--- src/com/company/MotorCycleNewsEvent.java
+++ src/com/company/MotorCycleNewsEvent.java
@@ -2,6 +2,7 @@ package com.company;
 
 /**
  * Created by sylvain on 2016-12-10.
+ * Classe pour enregistrer les nouvelles dans le mode de la moto
  */
 public class MotorCycleNewsEvent extends EventBase implements IEvent{
 
