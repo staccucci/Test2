e489703115dc0bc9efa8a65231460ee967b2b035
Sylvain Taccucci <sylvain.taccucci@gmail.com>
2016-12-29 13:27:43 -0500

    Ajout d'un commentaire

--- src/com/company/EventGP.java
+++ src/com/company/EventGP.java
@@ -8,6 +8,7 @@ public class EventGP extends EventBase {
 
     /**
      * Constructeur pour tester encore une patente d'héritage
+     * Call super avec aucun arguments si aucun call super de spécifié
      */
     public EventGP() {
         System.out.println("Dans le constructure de EventGP");
